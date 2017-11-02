e20d520e5d2925dbecb73fca96f0567f3f88b85c
Trevor Hoy <trevorphoy@gmail.com>
2017-11-02 17:22:05 -0500

    updating final project to tictactoe

--- Intermediate_JavaScript/final_project/script.js
+++ Intermediate_JavaScript/final_project/script.js
@@ -1,12 +1,37 @@
-window.onload = function() {
 
-//create variables
-var words = ['monkey','hope','please','work'];
+var player = 1;
 
-var word = words[Math.floor(Math.random()*words.length)];
+var buttons = document.getElementsByTagName("button");
+
+var row1 = document.getElementById("row1");
+var row2 = document.getElementById("row2");
+var row3 = document.getElementById("row3");
 
 
+for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function(event){
+  
+  if(player == 1) {
+    event.target.innerHTML = "x";
+    event.target.disabled = "disabled";
+    player += 1;
+    winner();
+  } else {
+    event.target.innerHTML = "o";
+    event.target.disabled = "disabled";
+    player -= 1;
+    winner();
+    }
+   })
+} 
 
 
+function winner() {
+  if(row1.children[0].innerHTML == "o" && row1.children[1].innerHTML == "o" && row1.children[2].innerHTML == "o" || row2.children[0].innerHTML == "o" && row2.children[1].innerHTML == "o" && row2.children[2].innerHTML == "o" || row3.children[0].innerHTML == "o" && row3.children[1].innerHTML == "o" && row3.children[2].innerHTML == "o") {
+    alert("WINNER is O");
+  }
+}
+
+function reset() {
 
 }
