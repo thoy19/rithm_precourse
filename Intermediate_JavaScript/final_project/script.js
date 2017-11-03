53c0524489773c905895643e77a08278c8a45f7f
Trevor Hoy <trevorphoy@gmail.com>
2017-11-03 10:56:35 -0500

    added reset button

--- Intermediate_JavaScript/final_project/script.js
+++ Intermediate_JavaScript/final_project/script.js
@@ -1,18 +1,19 @@
 
 var player = 1;
 
-var buttons = document.getElementsByTagName("button");
+var buttons = document.getElementsByClassName("tic");
 
 var row1 = document.getElementById("row1");
 var row2 = document.getElementById("row2");
 var row3 = document.getElementById("row3");
-
+var winnerBox = document.getElementById("result");
+var resetBtn = document.getElementById("reset");
 
 for(var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function(event){
   
   if(player == 1) {
-    event.target.innerHTML = "x";
+    event.target.innerText = "x";
     event.target.disabled = "disabled";
     player += 1;
     winner();
@@ -28,14 +29,16 @@ for(var i = 0; i < buttons.length; i++) {
 
 function winner() {
   if(row1.children[0].innerHTML == "o" && row1.children[1].innerHTML == "o" && row1.children[2].innerHTML == "o" || row2.children[0].innerHTML == "o" && row2.children[1].innerHTML == "o" && row2.children[2].innerHTML == "o" || row3.children[0].innerHTML == "o" && row3.children[1].innerHTML == "o" && row3.children[2].innerHTML == "o" || row1.children[0].innerHTML == "x" && row1.children[1].innerHTML == "x" && row1.children[2].innerHTML == "x" || row2.children[0].innerHTML == "x" && row2.children[1].innerHTML == "x" && row2.children[2].innerHTML == "x" || row3.children[0].innerHTML == "x" && row3.children[1].innerHTML == "x" && row3.children[2].innerHTML == "x") {
-    alert("WINNER");
+    winnerBox.innerText = "We have a winner!";
   } else if (row1.children[0].innerHTML == "o" && row2.children[0].innerHTML == "o" && row3.children[0].innerHTML == "o" || row1.children[1].innerHTML == "o" && row2.children[1].innerHTML == "o" && row3.children[1].innerHTML == "o" || row1.children[2].innerHTML == "o" && row2.children[2].innerHTML == "o" && row3.children[2].innerHTML == "o" || row1.children[0].innerHTML == "x" && row2.children[0].innerHTML == "x" && row3.children[0].innerHTML == "x" || row1.children[1].innerHTML == "x" && row2.children[1].innerHTML == "x" && row3.children[1].innerHTML == "x" || row1.children[2].innerHTML == "x" && row2.children[2].innerHTML == "x" && row3.children[2].innerHTML == "x") {
-    alert("winner column");
+    winnerBox.innerText = "We have a winner!";
   } else if (row1.children[0].innerHTML == "o" && row2.children[1].innerHTML == "o" && row3.children[2].innerHTML == "o" || row1.children[2].innerHTML == "o" && row2.children[1].innerHTML == "o" && row3.children[0].innerHTML == "o" || row1.children[0].innerHTML == "x" && row2.children[1].innerHTML == "x" && row3.children[2].innerHTML == "x" || row1.children[2].innerHTML == "x" && row2.children[1].innerHTML == "x" && row3.children[0].innerHTML == "x") {
-    alert("Winner diag")
+    winnerBox.innerText = "We have a winner!";
   }
 }
 
-function reset() {
-
-}
+resetBtn.addEventListener("click", function() {
+  for (var k = 0; k < buttons.length; k++) {
+    buttons[k].innerHTML = '';
+  }
+})
